dfe363cff3596a82ffe96ab71b024e0ea4ee1f2d
Wouman <119062277+Wouman@users.noreply.github.com>
2022-11-25 23:38:02 +0700

    Update LinuxStress.cpp

--- LinuxStress.cpp
+++ LinuxStress.cpp
@@ -2,24 +2,22 @@
 #include <stdlib.h>
 #include <iostream>
 
-#define VERSION "1.0-beta"
-
+#define VERSION "1.0"
 using namespace std;
 
-int threads_num;
-
 int main() {
-	cout << "=======| Starting LINUX STRESS " << VERSION " |======="<< endl;
+	cout << "=======| Starting LINUX STRESS " << VERSION " |=======" << endl;
 	cout << endl;
 
-	cout << "Enter number of threads (Warning! Overdoing it can cause freezes): ";
+	int threads_num;
+
+	cout << "Enter the number of threads (Warning! Overdoing it can cause freezes): ";
 	cin  >> threads_num;
 
 	omp_set_num_threads(threads_num);
-	
-	#pragma omp parallel 
-	{
 
+	#pragma omp parallel
+	{
 		int s;
 		for (int i = 0; i < 5000; i++)
 		{
